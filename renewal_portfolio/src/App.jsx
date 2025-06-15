6d9707f52241c52c0547c8e8b46dc813623f589f
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2025-06-15 23:13:23 +0900

    feat: set up Vite + React environment

---
+++ renewal_portfolio/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>포트폴리오</h1>
+      </header>
+      <main>
+        <section>
+          <h2>프로젝트</h2>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; 2025 EUNSUJEONG. All Rights Reserved.</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
