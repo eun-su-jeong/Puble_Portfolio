280b253914d5177529bdc8c6c675a07f76ec4af1
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2025-06-16 23:40:51 +0900

    feat: main detail

--- renewal_portfolio/src/components/Text/TextSlice.jsx
+++ renewal_portfolio/src/components/Text/TextSlice.jsx
@@ -1,10 +1,11 @@
-const TextSlice = ({text, highlihgt = [] }) => {
+import styles from "./TextSlice.module.scss";
+const TextSlice = ({text, highlight = [] }) => {
     return(
         <div className={styles.justifyLine}>
             {text.split("").map((char, index) => (
                 <span
                     key={index}
-                    className={highlihgt.includes(index)? styles.over : ""}
+                    className={highlight.includes(char)? styles.over : ""}
                 >
                     {char}
                 </span>
