8fd0e1f662d7063e97caf6ca00fcda62d8699aab
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2025-06-16 18:29:54 +0900

    feat: intro, header, logo component

--- renewal_portfolio/src/App.jsx
+++ renewal_portfolio/src/App.jsx
@@ -1,18 +1,28 @@
+import './styles/global.scss';
+import Header from "@/components/Header/Header.jsx";
+import {useState} from "react";
+import Intro from "@/components/Intro/Intro.jsx";
+
 function App() {
+    const [showIntro, setShowIntro] = useState(true);
 
   return (
     <>
-      <header>
-        <h1>포트폴리오</h1>
-      </header>
-      <main>
-        <section>
-          <h2>프로젝트</h2>
-        </section>
-      </main>
-      <footer>
-        <p>&copy; 2025 EUNSUJEONG. All Rights Reserved.</p>
-      </footer>
+        {showIntro ?(
+            <Intro onFinish={() => setShowIntro(false)} />
+        ):(
+           <>
+               <Header />
+               <main>
+                   <section>
+                       <h2>프로젝트</h2>
+                   </section>
+               </main>
+               <footer>
+                   <p>&copy; 2025 EUNSUJEONG. All Rights Reserved.</p>
+               </footer>
+           </>
+        )}
     </>
   )
 }
